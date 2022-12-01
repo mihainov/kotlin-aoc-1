5d8cca9343e85c75ce3a25b041f52a8a4975ccb0
mihailmarinov <genderlik@gmail.com>
2022-12-02 00:49:05 +0100

    Add first solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,19 @@
+import kotlin.math.max
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxSum: Int = -1
+        var tempSum = 0
+        input.forEach { inputString ->
+            if (inputString.isBlank()) {
+                maxSum = max(tempSum, maxSum)
+                tempSum = 0
+                return@forEach
+            }
+
+            tempSum += inputString.toInt()
+        }
+        return maxSum
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +22,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24_000)
 
     val input = readInput("Day01")
     println(part1(input))
