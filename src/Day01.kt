7c301e34151cc64291f752495225072544d8a194
mihailmarinov <genderlik@gmail.com>
2022-12-02 00:49:28 +0100

    Add second solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -17,12 +17,28 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elvesSum = mutableListOf<Int>()
+        var tempSum = 0
+        input.forEach{ inputString ->
+            if (inputString.isBlank()) {
+                elvesSum.add(tempSum)
+                tempSum = 0
+                return@forEach
+            }
+
+            tempSum += inputString.toInt()
+        }
+        // handle last elf's calories
+        elvesSum.add(tempSum)
+
+        elvesSum.sortDescending()
+        return elvesSum.subList(0, 3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 24_000)
+    check(part2(testInput) == 45_000)
 
     val input = readInput("Day01")
     println(part1(input))
